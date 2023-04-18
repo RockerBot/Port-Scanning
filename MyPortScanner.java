fd426c112dce31d9cafcc68063b86b7b91d99fb9
RockerBot <rockerbotmail@gmail.com>
2023-04-18 21:50:34 +0530

    prints matrix for open ports

--- MyPortScanner.java
+++ MyPortScanner.java
@@ -1,66 +1,40 @@
-// import java.io.*;
-// import java.net.*;
-// public class MyPortScanner{
-//     public static void main(String args[]){
-//         System.out.println("Hello");
-//     }
-//     public void runPortScan(String ip, int nbrPortMaxToScan) throws IOException {
-//         ConcurrentLinkedQueue openPorts = new ConcurrentLinkedQueue<>();
-//         ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
-//         AtomicInteger port = new AtomicInteger(0);
-//         while (port.get() < nbrPortMaxToScan) {
-//                 final int currentPort = port.getAndIncrement();
-//                 executorService.submit(() -> {
-//                         try {
-//                                 Socket socket = new Socket();
-//                                 socket.connect(new InetSocketAddress(ip, currentPort), timeOut);
-//                                 socket.close();
-//                                 openPorts.add(currentPort);
-//                                 System.out.println(ip + " ,port open: " + currentPort);
-//                         } catch (IOException e) {
-//                                 System.err.println(e);
-//                         }
-//                 });
-//         }
-//         executorService.shutdown();
-//         try {
-//                 executorService.awaitTermination(10, TimeUnit.MINUTES);
-//         } catch (InterruptedException e) {
-//                 throw new RuntimeException(e);
-//         }
-//         List openPortList = new ArrayList<>();
-//         System.out.println("openPortsQueue: " + openPorts.size());
-//         while (!openPorts.isEmpty()) {
-//                 openPortList.add(openPorts.poll());
-//         }
-//         openPortList.forEach(p -> System.out.println("port " + p + " is open"));
-// }
-// }
-
-
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
  
 class MyPortScanner {
     public static void main(String []args) {
+        ArrayList<Integer> ports = new ArrayList<Integer>();
+        boolean A[][] = new boolean[256][256];
+        for(int i=0;i<256;i++)for(int j=0;j<256;j++)A[i][j]=false;
         // Scanner sc = new Scanner(System.in);
         String hostname = "localhost";
         // System.out.println("Enter hostname pls");
         // hostname = sc.next();
         // sc.close();
         for (int port = 1; port <= 65535; port++) {
+            // System.out.println(" "+(port>>8)+" "+(port&255));
             try {
                 Socket socket = new Socket();
                 socket.connect(new InetSocketAddress(hostname, port), 1000);
                 socket.close();
                 System.out.println("Port " + port + " is open");
+                A[port>>8][port&255] = true;
+                ports.add(port);
             } catch (SocketException e){
-                // System.out.println("Port " + port + " is closed");
+                if ((port&4095) == 0) System.out.println("Port " + port + " is closed");
             } catch (Exception e) {
-                // e.printStackTrace();
+                e.printStackTrace();
                 // break;
             }
         }
+        System.out.println(" "+ports.size()+" "+ports);
+        // System.out.println(""+A);
+        // for (boolean[] row: A){
+        //     for (boolean i: row)
+        //         System.out.print(""+(i?1:0));
+        //     System.out.println();
+        // }
     }
 }
  
